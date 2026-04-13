8e3e88dadc4062f2b09449ef262f0f315f053bc9
rohanre1813 <rohanshetty135781@gmail.com>
2026-04-13 22:55:35 +0530

    replacing uuid by nanoid

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import crypto from "crypto";
+import { nanoid } from "nanoid";
 import User from "../models/user.js";
 
 const isProd = process.env.NODE_ENV === "production";
@@ -21,7 +22,7 @@ export const register = async (req, res) => {
 
     const user = await User.create({
       name, email, password: hashed, mobile, companyName,
-      shopId: crypto.randomUUID()
+      shopId: nanoid(10) // Beautiful 10-character shopId
     });
 
     const token = jwt.sign(
